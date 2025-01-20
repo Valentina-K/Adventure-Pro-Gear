8370f0ba8cf9731c79885c495a37b5a8124d7081
Valentina-K <kvalentyna@hotmail.com>
2025-01-20 13:59:36 +0200

    refactor: add link to the card, refactor code to fix eslint problems

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { Locale } from '@/i18n-config';
 import FollowinIcon from '@/../public/icons/Following.svg';
 import FollowingFill from '@/../public/icons/FollowingFill.svg';
@@ -35,6 +36,17 @@ interface CardProps {
   product: Product;
 }
 
+const getClassName = (variant: string) => {
+  switch (variant) {
+    case 'big':
+      return `${styles.cardWrapper} ${styles.big}`;
+    case 'small':
+      return `${styles.cardWrapper} ${styles.small}`;
+    default:
+      return styles.cardWrapper;
+  }
+};
+
 const Card: React.FC<CardProps> = ({
   product,
   attrInd = 0,
@@ -55,6 +67,7 @@ const Card: React.FC<CardProps> = ({
   const [following, setFollowing] = useState(FollowinIcon);
   const productImage =
     product.contents.length > 0 ? product.contents[0].source : 'https://dummyimage.com/180x180';
+  let className = getClassName(variant);
 
   useEffect(() => {
     setNewPrice(
@@ -62,24 +75,12 @@ const Card: React.FC<CardProps> = ({
     );
     setProductName(locale === 'uk-UA' ? product.productNameUa : product.productNameEn);
     setIsAvailable(product.attributes[attrInd].quantity > 0);
-    setClassNameImg(
-      isAvailable
-        ? variant === 'big'
-          ? `${styles.imageWrapper} ${styles.big}`
-          : variant === 'small'
-            ? `${styles.imageWrapper} ${styles.small}`
-            : `${styles.imageWrapper}`
-        : variant === 'big'
-          ? `${styles.imageWrapper} ${styles.big} ${styles.outStock}`
-          : variant === 'small'
-            ? `${styles.imageWrapper} ${styles.small} ${styles.outStock}`
-            : `${styles.imageWrapper} ${styles.outStock}`
-    );
+    setClassNameImg(isAvailable ? className : `${className} ${styles.outStock}`);
 
     if (addToFavorite && isLogged) {
       setFollowing(FollowingFill);
     } else setFollowing(FollowinIcon);
-  }, [product, locale, addToFavorite, isAvailable, variant, isLogged, attrInd]);
+  }, [product, locale, addToFavorite, isAvailable, variant, isLogged, attrInd, className]);
 
   const handleAddToFavorite: (event: any) => void = () => {
     setAddToFavorite(!addToFavorite);
@@ -88,120 +89,121 @@ const Card: React.FC<CardProps> = ({
 
   return (
     <section
-      className={
-        variant === 'big'
-          ? `${styles.cardWrapper} ${styles.big}`
-          : variant === 'small'
-            ? `${styles.cardWrapper} ${styles.small}`
-            : `${styles.cardWrapper}`
-      } /* onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave} */
+      className={className}
+      /* onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave} */
     >
-      <div className={classNameImg}>
-        {addToFavorite && !isLogged && (
+      <Link href={`/product/${product.productId}`}>
+        <div className={classNameImg}>
+          {addToFavorite && !isLogged && (
+            <div
+              className={
+                variant === 'big'
+                  ? `${styles.addToFavorite} ${styles.big}`
+                  : `${styles.addToFavorite}`
+              }
+            >
+              {translation.card.addToFollowing}
+            </div>
+          )}
+          <Image
+            className={styles.productImage}
+            src={productImage}
+            layout="fill"
+            objectFit="contain"
+            alt={productName}
+          />
           <div
             className={
               variant === 'big'
-                ? `${styles.addToFavorite} ${styles.big}`
-                : `${styles.addToFavorite}`
+                ? `${styles.specialWrapper} ${styles.big}`
+                : `${styles.specialWrapper}`
             }
           >
-            {translation.card.addToFollowing}
+            {!isAvailable && (
+              <Image
+                className={styles.saleOut}
+                src={NotAvailable}
+                alt="not available"
+                width={105}
+                height={49}
+              />
+            )}
+            {product.basePrice > newPrice && (
+              <div className={styles.sale}>{translation.card.sale}</div>
+            )}
+            {product.attributes[0].label && (
+              <div className={styles.new}>{translation.card.new}</div>
+            )}
           </div>
-        )}
-        <Image
-          className={styles.productImage}
-          src={productImage}
-          layout="fill"
-          objectFit="contain"
-          alt={productName}
-        />
-        <div
-          className={
-            variant === 'big'
-              ? `${styles.specialWrapper} ${styles.big}`
-              : `${styles.specialWrapper}`
-          }
-        >
-          {!isAvailable && (
-            <Image
-              className={styles.saleOut}
-              src={NotAvailable}
-              alt="not available"
-              width={105}
-              height={49}
-            />
-          )}
-          {product.basePrice > newPrice && (
-            <div className={styles.sale}>{translation.card.sale}</div>
-          )}
-          {product.attributes[0].label && <div className={styles.new}>{translation.card.new}</div>}
+          <button onClick={handleAddToFavorite} className={styles.following}>
+            <Image src={following} width={20} height={18} alt="following" />
+          </button>
         </div>
-        <button onClick={handleAddToFavorite} className={styles.following}>
-          <Image src={following} width={20} height={18} alt="following" />
-        </button>
-      </div>
-      <div className={styles.cardContent}>
-        <div className={styles.cardInfo}>
-          <h3
-            className={
-              variant === 'big' ? `${styles.productName} ${styles.big}` : `${styles.productName}`
-            }
-          >
-            {productName}
-          </h3>
-          <div className={styles.rating}>
-            <RatingStars averageRating={avgRating} />
-            <ReviewCount reviewCount={countReviews} />
-          </div>
-          {product.basePrice !== newPrice ? (
-            <div className={styles.price}>
-              <div className={styles.oldPrice}>
-                <span>{product.basePrice} ₴</span>
-                <span
+        <div className={styles.cardContent}>
+          <div className={styles.cardInfo}>
+            <h3
+              className={
+                variant === 'big' ? `${styles.productName} ${styles.big}` : `${styles.productName}`
+              }
+            >
+              {productName}
+            </h3>
+            <div className={styles.rating}>
+              <RatingStars averageRating={avgRating} />
+              <ReviewCount reviewCount={countReviews} />
+            </div>
+            {product.basePrice !== newPrice ? (
+              <div className={styles.price}>
+                <div className={styles.oldPrice}>
+                  <span>{product.basePrice} ₴</span>
+                  <span
+                    className={
+                      variant === 'big'
+                        ? `${styles.deviation} ${styles.big}`
+                        : `${styles.deviation}`
+                    }
+                  >
+                    {product.attributes[attrInd].priceDeviation} %
+                  </span>
+                </div>
+                <div
                   className={
-                    variant === 'big' ? `${styles.deviation} ${styles.big}` : `${styles.deviation}`
+                    variant === 'big' ? `${styles.newPrice} ${styles.big}` : `${styles.newPrice}`
                   }
                 >
-                  {product.attributes[attrInd].priceDeviation} %
-                </span>
+                  {newPrice} ₴
+                </div>
               </div>
+            ) : (
               <div
                 className={
                   variant === 'big' ? `${styles.newPrice} ${styles.big}` : `${styles.newPrice}`
                 }
               >
-                {newPrice} ₴
+                {product.basePrice} ₴
               </div>
-            </div>
-          ) : (
-            <div
-              className={
-                variant === 'big' ? `${styles.newPrice} ${styles.big}` : `${styles.newPrice}`
-              }
-            >
-              {product.basePrice} ₴
-            </div>
-          )}
+            )}
+          </div>
+          <div
+            className={
+              variant === 'big' ? `${styles.available} ${styles.big}` : `${styles.available}`
+            }
+          >
+            {isAvailable ? translation.card.available : translation.card.outOfStock}
+          </div>
         </div>
-        <div
-          className={
-            variant === 'big' ? `${styles.available} ${styles.big}` : `${styles.available}`
-          }
-        >
-          {isAvailable ? translation.card.available : translation.card.outOfStock}
+        <div className={styles.buySection}>
+          <Button
+            onClick={() => onBuyClick(product.productId)}
+            text={translation.card.buy}
+            className={
+              variant === 'big' ? `${styles.buyButton} ${styles.big}` : `${styles.buyButton}`
+            }
+            disabled={!isAvailable}
+            icon={<Image src={Comercial} width={20} height={20} alt="Comercial" />}
+          />
         </div>
-      </div>
-      <div className={styles.buySection}>
-        <Button
-          onClick={() => onBuyClick(product.productId)}
-          text={translation.card.buy}
-          className={
-            variant === 'big' ? `${styles.buyButton} ${styles.big}` : `${styles.buyButton}`
-          }
-          disabled={!isAvailable}
-          icon={<Image src={Comercial} width={20} height={20} alt="Comercial" />}
-        />
-      </div>
+      </Link>
     </section>
   );
 };
