0ab6c82a1801e5e1632c908093610ea9c5133a33
0v3nb1rd <0v3nb1rd@gmail.com>
2025-02-06 00:12:51 +0200

    UI: hover-link disable without link

--- src/components/SocialLinks/index.tsx
+++ src/components/SocialLinks/index.tsx
@@ -6,7 +6,7 @@ import style from './SocialLinks.module.css';
 const SocialLinks = ({ className }: { className?: string }) => (
   <ul className={clsx(className, style.socials)}>
     <li>
-      <Link href="/" rel="noopener noreferrer">
+      <Link href="" rel="noopener noreferrer">
         <svg
           width="24"
           height="24"
@@ -36,7 +36,7 @@ const SocialLinks = ({ className }: { className?: string }) => (
       </Link>
     </li>
     <li>
-      <Link href="/" rel="noopener noreferrer">
+      <Link href="" rel="noopener noreferrer">
         <svg
           width="24"
           height="24"
@@ -58,7 +58,7 @@ const SocialLinks = ({ className }: { className?: string }) => (
       </Link>
     </li>
     <li>
-      <Link href="/" rel="noopener noreferrer">
+      <Link href="" rel="noopener noreferrer">
         <svg
           width="24"
           height="24"
@@ -86,7 +86,7 @@ const SocialLinks = ({ className }: { className?: string }) => (
       </Link>
     </li>
     <li>
-      <Link href="/" rel="noopener noreferrer">
+      <Link href="" rel="noopener noreferrer">
         <svg
           width="24"
           height="24"
