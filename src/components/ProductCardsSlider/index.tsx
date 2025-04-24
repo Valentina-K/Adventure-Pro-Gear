e4082ba0412e3d723b258439355a9e32a7ce9593
Valentina-K <kvalentyna@hotmail.com>
2025-04-24 16:19:10 +0300

    add two selector for filtered products, update productCardsSlider

--- src/components/ProductCardsSlider/index.tsx
+++ src/components/ProductCardsSlider/index.tsx
@@ -6,77 +6,54 @@ import { useLocale, useTranslations } from 'next-intl';
 import Card from '../Card';
 import styles from './ProductCardsSlider.module.css';
 
-
 interface CardsSliderProp {
   products: Product[];
   recommendation?: Product[];
   title: string;
 }
 
-const ProductCardsSlider: React.FC<CardsSliderProp> = ({
-  products,
-  title,
-  recommendation,
-}) => {
+const ProductCardsSlider: React.FC<CardsSliderProp> = ({ products, title, recommendation }) => {
   const locale = useLocale();
   const t = useTranslations('product');
-  const [currentIndex, setCurrentIndex] = useState(0);
   const [activeNav, setActiveNav] = useState(0);
 
-  const handleNavClick = (index: number) => {
-    if (activeNav === 0) setCurrentIndex(prevIndex => prevIndex + index);
-    else setCurrentIndex(prevIndex => prevIndex - 1);
-    setActiveNav(index);
-  };
-
-  const nextSlide = (index: number) => {
-    setActiveNav(index);
-    if (currentIndex < products.length - 3) setCurrentIndex(prevIndex => prevIndex + 1);
-    else setCurrentIndex(products.length - 3);
-  };
-  const prevSlide = (index: number) => {
-    setActiveNav(index);
-    if (currentIndex === 0) setCurrentIndex(0);
-    else setCurrentIndex(prevIndex => prevIndex - 1);
-  };
-
-  const getVisibleSlides = () => {
-    const start = currentIndex;
-    const end = currentIndex + 3;
-    return products?.slice(start, end);
-  };
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>{title}</h2>
       {!recommendation && (
         <div className={styles.slider}>
-          <div className={styles.slides}>
-            {getVisibleSlides()?.map((slide, index) => (
-              <div key={index} className={styles.slide}>
-                {slide.attributes.length > 0 && (
-                  <Card
-                    product={slide}
-                    variant='small'
-                  />
-                )}
-              </div>
-            ))}
+          <div className={styles.slides} style={{ transform: `translateX(-${activeNav * 33.33}%)` }}>
+            <div className={styles.slide}>
+              {products.slice(0, 3).map(item => (
+                <Card key={item.productId} product={item} variant="small" />
+              ))}
+            </div>
+            <div className={styles.slide}>
+              {products.slice(3, 6).map(item => (
+                <Card key={item.productId} product={item} variant="small" />
+              ))}
+            </div>
+            <div className={styles.slide}>
+              {products.slice(6, 9).map(item => (
+                <Card key={item.productId} product={item} variant="small" />
+              ))}
+            </div>
           </div>
           <div className={styles.nav}>
             <button
               className={activeNav === 0 ? styles.active : ''}
-              onClick={() => prevSlide(0)}
+              onClick={() => setActiveNav(0)}
               aria-label="Previous Slide"
             />
             <button
               className={activeNav === 1 ? styles.active : ''}
-              onClick={() => handleNavClick(1)}
+              onClick={() => setActiveNav(1)}
               aria-label="Next slide"
             />
             <button
               className={activeNav === 2 ? styles.active : ''}
-              onClick={() => nextSlide(2)}
-              aria-label="Next-slide"
+              onClick={() => setActiveNav(2)}
+              aria-label="Next slide"
             />
           </div>
         </div>
@@ -87,9 +64,7 @@ const ProductCardsSlider: React.FC<CardsSliderProp> = ({
           <div className={styles.slides}>
             {recommendation?.map((slide, index) => (
               <div key={index} className={styles.slide}>
-                <Card
-                  product={slide}
-                />
+                <Card product={slide} />
               </div>
             ))}
           </div>
@@ -100,3 +75,36 @@ const ProductCardsSlider: React.FC<CardsSliderProp> = ({
 };
 
 export default ProductCardsSlider;
+/* <div class="carousel">
+        <input type="radio" name="slider" id="slide1" checked>
+        <input type="radio" name="slider" id="slide2">
+        <input type="radio" name="slider" id="slide3">
+{products?.map((slide, index) => (
+              <div key={index} className={styles.slide}>
+                {slide.attributes.length > 0 && (
+                  <Card
+                    product={slide}
+                    variant='small'
+                  />
+                )}
+              </div>
+            ))}
+        <div class="slides">
+            <div class="slide">
+
+                <img src="image1.jpg" alt="Image 1" style="width:100%">
+            </div>
+            <div class="slide">
+                <img src="image2.jpg" alt="Image 2" style="width:100%">
+            </div>
+            <div class="slide">
+                <img src="image3.jpg" alt="Image 3" style="width:100%">
+            </div>
+        </div>
+
+        <div class="controls">
+            <label for="slide1" class="control">1</label>
+            <label for="slide2" class="control">2</label>
+            <label for="slide3" class="control">3</label>
+        </div>
+    </div> */
--- src/components/ReviewedGoods/index.tsx
+++ src/components/ReviewedGoods/index.tsx
@@ -4,11 +4,9 @@ import styles from './ReviewedGoods.module.css';
 
 interface ReviewedGoodsProps {
   title: string;
-  onBuyClick: (id: number) => void;
-  onFavoriteClick: (productId: number, isFavorite: boolean) => void;
 }
 
-function ReviewedGoods({ title, onBuyClick, onFavoriteClick }: ReviewedGoodsProps) {
+function ReviewedGoods({ title }: ReviewedGoodsProps) {
   const products = useAppSelector(state => state.products.reviewedProducts);
   return (
     <section className={styles.wrapper}>
