bac4f4976f52cccfd092ff466c3572232407307e
Valentina-K <kvalentyna@hotmail.com>
2025-06-03 16:25:28 +0300

    fix bag

--- src/dictionaries/dictionaries.ts
+++ src/dictionaries/dictionaries.ts
@@ -4,6 +4,7 @@ import { i18n, Locale } from '../i18n-config';
 const languages = {
   [i18n.locales[0]]: () => import('./uk.json').then(module => module.default),
   [i18n.locales[1]]: () => import('./en.json').then(module => module.default),
+  [i18n.locales[2]]: () => import('./uk.json').then(module => module.default),
 };
 
 export const getAllTranslations = async (locale: Locale) => {
